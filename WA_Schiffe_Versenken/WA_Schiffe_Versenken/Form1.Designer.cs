f6f8e2db54c598a96954f676c3068607626e6d23
Sven Greinert <svengreinert@outlook.com>
2018-11-29 11:06:36 +0100

    Initial Commit 2

---
+++ WA_Schiffe_Versenken/WA_Schiffe_Versenken/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WA_Schiffe_Versenken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.canvas_enemy = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas_enemy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // canvas_enemy
+            // 
+            this.canvas_enemy.BackColor = System.Drawing.Color.White;
+            this.canvas_enemy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas_enemy.Location = new System.Drawing.Point(480, 54);
+            this.canvas_enemy.Name = "canvas_enemy";
+            this.canvas_enemy.Size = new System.Drawing.Size(350, 350);
+            this.canvas_enemy.TabIndex = 1;
+            this.canvas_enemy.TabStop = false;
+            this.canvas_enemy.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            this.canvas_enemy.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_enemy_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(632, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Computer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(196, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Spieler";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(420, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Züge\r\n0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 492);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.canvas_enemy);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas_enemy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox canvas_enemy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WA_Schiffe_Versenken/WA_Schiffe_Versenken/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WA_Schiffe_Versenken
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct shipType
+        {
+            string name;
+            int length;
+            int amount;
+            int id;
+            private List<int> shipsVisible;
+
+            public string Name { get => name; set => name = value; }
+            public int Length { get => length; set => length = value; }
+            public int Amount { get => amount; set => amount = value; }
+            public int Id { get => id; set => id = value; }
+            public List<int> ShipsVisible { get => shipsVisible; set => shipsVisible = value; }
+        }
+
+        shipType[] userShips = new shipType[4];
+        shipType[] enemyShips = new shipType[4];
+
+        enum CellType
+        {
+            Wasser,
+            Schlachtschiff,
+            Kreuzer,
+            Zerstörer,
+            UBoot
+        }
+
+        int züge = 0;
+        int[,] hits = new int[10,10];
+        CellType[,] userCells = new CellType[10,10];
+        CellType[,] enemyCells = new CellType[10,10];
+
+        List<Point> locationValid(Point p, int length, int dir)
+        {
+            bool ret = true;
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(dir + " - " + length + " + " + p);
+                switch (dir)
+                {
+                    case 1:
+                        try
+                        {
+                            if (enemyCells[p.X, p.Y + i] == CellType.Wasser)
+                            {
+                                Point pt = new Point(p.X, p.Y + i);
+                                points.Add(pt);
+                            }
+                            else
+                            {
+                                points.Clear(); break;
+                            }
+                        }  catch (Exception)  { points.Clear(); break;  }
+                        break;
+                    case 2:
+                        try
+                        {
+                            if (enemyCells[p.X - i, p.Y] == CellType.Wasser)
+                            {
+                                Point pt = new Point(p.X - i, p.Y);
+                                points.Add(pt);
+                            }
+                            else
+                            {
+                                points.Clear(); break;
+                            }
+                        }  catch (Exception) { points.Clear(); break; }
+                break;
+                    case 3:
+                        try
+                        {
+                            if (enemyCells[p.X, p.Y - i] == CellType.Wasser)
+                            {
+                                Point pt = new Point(p.X, p.Y - i);
+                                points.Add(pt);
+                            }
+                            else
+                            {
+                                points.Clear(); break;
+                            }
+                        }  catch (Exception) { points.Clear(); break; }
+                break;
+                    case 4:
+                        try
+                        {
+                            if (enemyCells[p.X + i, p.Y] == CellType.Wasser)
+                            {
+                                Point pt = new Point(p.X + i, p.Y);
+                                points.Add(pt);
+                            }
+                            else
+                            {
+                                points.Clear(); break;
+                            }
+                        }  catch (Exception) { points.Clear(); break; }
+                        break;
+                }
+            }
+            if (points.Count < length)
+            {
+                points.Clear();
+            }
+            return points;
+        }
+
+        void generateEnemy()
+        {
+            Random r = new Random();
+            foreach (var item in enemyShips)
+            {
+                for (int i = 0; i < item.Amount; i++)
+                {
+                    Point newP = new Point(r.Next(0, 11), r.Next(0, 11));
+                    List<Point> points = new List<Point>();
+                    while (points.Count <= 0)
+                    {
+                        points = locationValid(newP, item.Length, r.Next(1, 5));
+                        if (points.Count <= 0) newP = new Point(r.Next(0, 11), r.Next(0, 11));
+                    }
+                    for (int j = 0; j < points.Count; j++)
+                    {
+                        enemyCells[points[j].X, points[j].Y] = (CellType)item.Id;
+                    }
+                }
+            }
+            canvas_enemy.Invalidate();
+        }
+
+        Point locationToCell(Point mouseLoc)
+        {
+            Point ret = new Point();
+            int length = canvas_enemy.Size.Width / enemyCells.GetLength(1);
+            int currX = 0, currY = 0;
+            for (int i = 0; i < enemyCells.GetLength(0); i++)
+            {
+                for (int j = 0; j < enemyCells.GetLength(1); j++)
+                {
+                    if ((new Rectangle(currX, currY, length, length)).Contains(mouseLoc))
+                    {
+                        ret = new Point(i, j);
+                    }
+                    currX += length;
+                }
+                currX = 0;
+                currY += length;
+            }
+            return ret;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userShips[0] = new shipType() { Name = "Schlachtschiff", Amount = 1, Length = 5, Id = 1, ShipsVisible = new List<int>() };
+            userShips[1] = new shipType() { Name = "Kreuzer", Amount = 2, Length = 4, Id = 2, ShipsVisible = new List<int>() };
+            userShips[2] = new shipType() { Name = "Zerstörer", Amount = 3, Length = 3, Id = 3, ShipsVisible = new List<int>() };
+            userShips[3] = new shipType() { Name = "UBoot", Amount = 4, Length = 2, Id = 4, ShipsVisible = new List<int>() };
+            enemyShips[0] = new shipType() { Name = "Schlachtschiff", Amount = 1, Length = 5, Id = 1, ShipsVisible = new List<int>() };
+            enemyShips[1] = new shipType() { Name = "Kreuzer", Amount = 2, Length = 4, Id = 2, ShipsVisible = new List<int>() };
+            enemyShips[2] = new shipType() { Name = "Zerstörer", Amount = 3, Length = 3, Id = 3, ShipsVisible = new List<int>() };
+            enemyShips[3] = new shipType() { Name = "UBoot", Amount = 4, Length = 2, Id = 4, ShipsVisible = new List<int>() };
+
+            generateEnemy();
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            int currX = 0, currY = 0;
+            int length = canvas_enemy.Size.Width / enemyCells.GetLength(0);
+            for (int i = 0; i < enemyCells.GetLength(0); i++)
+            {
+                for (int j  = 0; j < enemyCells.GetLength(1); j++)
+                {
+                    Color c = Color.MediumTurquoise;
+                    Color hitColor = Color.White;
+                    if (hits[i, j] == 2) hitColor = Color.Red;
+                    if (enemyCells[i, j] == CellType.Schlachtschiff) c = Color.Gray;
+                    if (enemyCells[i, j] == CellType.Kreuzer) c = Color.IndianRed;
+                    if (enemyCells[i, j] == CellType.Zerstörer) c = Color.PaleGreen;
+                    if (enemyCells[i, j] == CellType.UBoot) c = Color.DarkOrange;
+                    e.Graphics.FillRectangle(new SolidBrush(c), currX, currY, length, length);
+
+                    if (hits[i, j] > 0) e.Graphics.DrawEllipse(new Pen(hitColor, 2), currX + 5, currY + 5, length - 10, length - 10);
+
+                    currX += length;
+
+                }
+                currX = 0;
+                currY += length;
+            }
+        }
+
+        private void canvas_enemy_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point cellLoc = locationToCell(e.Location);
+            if (hits[cellLoc.X, cellLoc.Y] == 0)
+            {
+                if (enemyCells[cellLoc.X, cellLoc.Y] == CellType.Wasser)
+                {
+                    hits[cellLoc.X, cellLoc.Y] = 1;
+                }
+                else
+                {
+                    hits[cellLoc.X, cellLoc.Y] = 2;
+                }
+                züge++;
+                label3.Text = "Züge\n" + züge;
+                canvas_enemy.Invalidate();
+            }
+        }
+    }
+}
